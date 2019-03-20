e3f214b0e86123de86651f84c8d153d583157b48
Gergo  Csizmadia <gergo589@gmail.com>
2019-03-20 11:20:31 +0100

    newGirl

---
+++ Views/newGirl.php
@@ -0,0 +1,35 @@
+<form>
+    <div class="form-group">
+        <label for="name">Név</label>
+        <input type="text" class="form-control" id="name" name="name" placeholder="Enter the name of her">
+    </div>
+    <div class="form-group">
+        <label for="age">Kor</label>
+        <input type="number" class="form-control" id="age" name="age" placeholder="Enter the age of her">
+    </div>
+    <div class="form-group">
+        <label for="hairColour">Haj szín</label>
+        <input type="text" class="form-check-input" id="hairColour" name="hairColour" placeholder="Enter the Hair colour of her">
+    </div>
+    <div class="form-group">
+        <label for="hairColour">Szem szín</label>
+        <input type="text" class="form-check-input" id="hairColour" name="hairColour" placeholder="Enter the eyes colour of her">
+    </div>
+    <div class="form-group">
+        <label for="skinColour">Bőrszín</label>
+        <input type="text" class="form-check-input" id="skinColour" name="skinColour" placeholder="Enter the skin colour of her">
+    </div>
+    <div class="form-group">
+        <label for="breastSize">Mell méret</label>
+        <input type="text" class="form-check-input" id="breastSize" name="breastSize" placeholder="Enter the breast size of her">
+    </div>
+    <div class="form-group">
+        <label for="phisique">Fizikum</label>
+        <input type="text" class="form-check-input" id="phisique" name="phisique" placeholder="Enter the Phisique of her">
+    </div>
+    <div class="form-group">
+        <label for="height">Magasság</label>
+        <input type="text" class="form-check-input" id="height" name="height" placeholder="Enter the Height of her">
+    </div>
+    <button type="submit" id="submitForm" name="submitForm" class="btn btn-primary">Felvisz</button>
+    </form>
