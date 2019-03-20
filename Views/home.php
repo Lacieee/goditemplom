99766e562cc163ebe4c25cfcae16127f000a9990
Gergo  Csizmadia <gergo589@gmail.com>
2019-03-20 12:03:36 +0100

    asd

--- Views/home.php
+++ Views/home.php
@@ -12,7 +12,8 @@
     <div class="container">
         <div class="jumbotron">
             <h4 class="text-center">At the moment your are not logged in! <br> Please login to view the page content!</h4>
+            <a type="button" id="login" name="login" href="#" onclick="render('regilogin.php')" class="btn btn-primary btn-lg btn-block">Login!</a>
+
         </div>
-        <a type="button" id="login" name="login" href="#" onclick="render('regilogin.php')" class="btn btn-primary btn-lg btn-block">Login!</button>
-    </div>
+         </div>
 </div>
