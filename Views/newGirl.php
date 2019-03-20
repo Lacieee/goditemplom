12022df3e1fbe79a64a17501de426e78b245005c
Gergo  Csizmadia <gergo589@gmail.com>
2019-03-20 11:51:48 +0100

    jobb nyelv

--- Views/newGirl.php
+++ Views/newGirl.php
@@ -1,42 +1,42 @@
 <br>
 <div class="container">
     <div class="jumbotron main-wrapper">
-        <h1>Új lány felvétele</h1>
+        <h1>New girl</h1>
         <form>
             <div class="form-group">
-                <label for="name">Név</label>
+                <label for="name">Name</label>
                 <input type="text" class="form-control" id="name" name="name" placeholder="Enter the name of her">
             </div>
             <div class="form-group">
-                <label for="age">Kor</label>
+                <label for="age">Age</label>
                 <input type="number" class="form-control" id="age" name="age" placeholder="Enter the age of her">
             </div>
             <div class="form-group">
-                <label for="hairColour">Haj szín</label>
+                <label for="hairColour">Hair color</label>
                 <input type="text" class="form-control" id="hairColour" name="hairColour" placeholder="Enter the Hair colour of her">
             </div>
             <div class="form-group">
-                <label for="hairColour">Szem szín</label>
+                <label for="hairColour">Eye color</label>
                 <input type="text" class="form-control" id="hairColour" name="hairColour" placeholder="Enter the eyes colour of her">
             </div>
             <div class="form-group">
-                <label for="skinColour">Bőrszín</label>
+                <label for="skinColour">Skin color</label>
                 <input type="text" class="form-control" id="skinColour" name="skinColour" placeholder="Enter the skin colour of her">
             </div>
             <div class="form-group">
-                <label for="breastSize">Mell méret</label>
+                <label for="breastSize">Breast size</label>
                 <input type="text" class="form-control" id="breastSize" name="breastSize" placeholder="Enter the breast size of her">
             </div>
             <div class="form-group">
-                <label for="phisique">Fizikum</label>
+                <label for="phisique">Phisique</label>
                 <input type="text" class="form-control" id="phisique" name="phisique" placeholder="Enter the Phisique of her">
             </div>
             <div class="form-group">
-                <label for="height">Magasság</label>
+                <label for="height">Height</label>
                 <input type="text" class="form-control" id="height" name="height" placeholder="Enter the Height of her">
             </div>
             <div class="form-group text-center">
-            <button type="submit" id="submitForm" name="submitForm" class="btn btn-primary">Felvisz</button>
+            <button type="submit" id="submitForm" name="submitForm" class="btn btn-primary btn-lg btn-block">Upload</button>
             </div>
         </form>
     </div>
