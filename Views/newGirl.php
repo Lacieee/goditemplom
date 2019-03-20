bbc928ae8e9553544e95ed814a3cc015643163d0
Gergo  Csizmadia <gergo589@gmail.com>
2019-03-20 11:43:44 +0100

    idk

--- Views/newGirl.php
+++ Views/newGirl.php
@@ -1,37 +1,37 @@
 <div class="container">
-<form>
-    <div class="form-group">
-        <label for="name">Név</label>
-        <input type="text" class="form-control" id="name" name="name" placeholder="Enter the name of her">
-    </div>
-    <div class="form-group">
-        <label for="age">Kor</label>
-        <input type="number" class="form-control" id="age" name="age" placeholder="Enter the age of her">
-    </div>
-    <div class="form-group">
-        <label for="hairColour">Haj szín</label>
-        <input type="text" class="form-control" id="hairColour" name="hairColour" placeholder="Enter the Hair colour of her">
-    </div>
-    <div class="form-group">
-        <label for="hairColour">Szem szín</label>
-        <input type="text" class="form-control" id="hairColour" name="hairColour" placeholder="Enter the eyes colour of her">
-    </div>
-    <div class="form-group">
-        <label for="skinColour">Bőrszín</label>
-        <input type="text" class="form-control" id="skinColour" name="skinColour" placeholder="Enter the skin colour of her">
-    </div>
-    <div class="form-group">
-        <label for="breastSize">Mell méret</label>
-        <input type="text" class="form-control" id="breastSize" name="breastSize" placeholder="Enter the breast size of her">
-    </div>
-    <div class="form-group">
-        <label for="phisique">Fizikum</label>
-        <input type="text" class="form-control" id="phisique" name="phisique" placeholder="Enter the Phisique of her">
-    </div>
-    <div class="form-group">
-        <label for="height">Magasság</label>
-        <input type="text" class="form-control" id="height" name="height" placeholder="Enter the Height of her">
-    </div>
-    <button type="submit" id="submitForm" name="submitForm" class="btn btn-primary">Felvisz</button>
+    <form>
+        <div class="form-group">
+            <label for="name">Név</label>
+            <input type="text" class="form-control" id="name" name="name" placeholder="Enter the name of her">
+        </div>
+        <div class="form-group">
+            <label for="age">Kor</label>
+            <input type="number" class="form-control" id="age" name="age" placeholder="Enter the age of her">
+        </div>
+        <div class="form-group">
+            <label for="hairColour">Haj szín</label>
+            <input type="text" class="form-control" id="hairColour" name="hairColour" placeholder="Enter the Hair colour of her">
+        </div>
+        <div class="form-group">
+            <label for="hairColour">Szem szín</label>
+            <input type="text" class="form-control" id="hairColour" name="hairColour" placeholder="Enter the eyes colour of her">
+        </div>
+        <div class="form-group">
+            <label for="skinColour">Bőrszín</label>
+            <input type="text" class="form-control" id="skinColour" name="skinColour" placeholder="Enter the skin colour of her">
+        </div>
+        <div class="form-group">
+            <label for="breastSize">Mell méret</label>
+            <input type="text" class="form-control" id="breastSize" name="breastSize" placeholder="Enter the breast size of her">
+        </div>
+        <div class="form-group">
+            <label for="phisique">Fizikum</label>
+            <input type="text" class="form-control" id="phisique" name="phisique" placeholder="Enter the Phisique of her">
+        </div>
+        <div class="form-group">
+            <label for="height">Magasság</label>
+            <input type="text" class="form-control" id="height" name="height" placeholder="Enter the Height of her">
+        </div>
+        <button type="submit" id="submitForm" name="submitForm" class="btn btn-primary">Felvisz</button>
     </form>
-    </div>
+</div>
