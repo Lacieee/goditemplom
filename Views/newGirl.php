7189e61c10f3a4cc29f9eb349e5783bb70dddc89
Gergo  Csizmadia <gergo589@gmail.com>
2019-03-20 11:26:37 +0100

    newgirl fix

--- Views/newGirl.php
+++ Views/newGirl.php
@@ -9,27 +9,27 @@
     </div>
     <div class="form-group">
         <label for="hairColour">Haj szín</label>
-        <input type="text" class="form-check-input" id="hairColour" name="hairColour" placeholder="Enter the Hair colour of her">
+        <input type="text" class="form-control" id="hairColour" name="hairColour" placeholder="Enter the Hair colour of her">
     </div>
     <div class="form-group">
         <label for="hairColour">Szem szín</label>
-        <input type="text" class="form-check-input" id="hairColour" name="hairColour" placeholder="Enter the eyes colour of her">
+        <input type="text" class="form-control" id="hairColour" name="hairColour" placeholder="Enter the eyes colour of her">
     </div>
     <div class="form-group">
         <label for="skinColour">Bőrszín</label>
-        <input type="text" class="form-check-input" id="skinColour" name="skinColour" placeholder="Enter the skin colour of her">
+        <input type="text" class="form-control" id="skinColour" name="skinColour" placeholder="Enter the skin colour of her">
     </div>
     <div class="form-group">
         <label for="breastSize">Mell méret</label>
-        <input type="text" class="form-check-input" id="breastSize" name="breastSize" placeholder="Enter the breast size of her">
+        <input type="text" class="form-control" id="breastSize" name="breastSize" placeholder="Enter the breast size of her">
     </div>
     <div class="form-group">
         <label for="phisique">Fizikum</label>
-        <input type="text" class="form-check-input" id="phisique" name="phisique" placeholder="Enter the Phisique of her">
+        <input type="text" class="form-control" id="phisique" name="phisique" placeholder="Enter the Phisique of her">
     </div>
     <div class="form-group">
         <label for="height">Magasság</label>
-        <input type="text" class="form-check-input" id="height" name="height" placeholder="Enter the Height of her">
+        <input type="text" class="form-control" id="height" name="height" placeholder="Enter the Height of her">
     </div>
     <button type="submit" id="submitForm" name="submitForm" class="btn btn-primary">Felvisz</button>
     </form>
