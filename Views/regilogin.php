21027bc97fb4b936dd6431545590f608fa01eff5
Shika <zcmate@gmail.com>
2019-03-20 12:33:04 +0100

    regilogin popup+wireing

--- Views/regilogin.php
+++ Views/regilogin.php
@@ -1,5 +1,18 @@
+<?php
+include_once "Controllers/userController.php";
+
+if(isset($_POST["buttonlog"]))
+{
+	
+	$errormsg = Login($_POST["namelog"],$_POST["passwordlog"]);
+	
+	
+}
+
+?>
+<script>alert(<?php $errormsg; ?>);</script>
 <br>
-<section class="container bounceIn">
+<section class="container">
 		<div class="main-wrapper jumbotron text-center">
 		<br>
 		<div class="row">
@@ -21,7 +34,7 @@
     <div class="card">
       <div class="card-body">
 	  <h2>Login:</h2>
-	  <form action="includes/signupphp.php" method="POST" class="signup-form text-center">
+	  <form action="Controllers/userController.php" method="POST" class="signup-form text-center">
 		<input type="text" class="form-control" name="namelog" placeholder="Username"><br>
 		<input type="password" class="form-control" name="passwordlog" placeholder="Password "><br>
 		<button class="btn btn-primary" name="buttonlog">Log in</button>
